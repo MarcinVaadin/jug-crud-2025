0f426322639a678fcfcf24b10403aaadccdc01e1
marcin <marcin@vaadin.com>
2025-04-23 22:48:03 +0200

    Entity, repository and service

---
+++ src/main/java/com/example/application/data/PersonService.java
@@ -0,0 +1,21 @@
+package com.example.application.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(@Autowired PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
+}
